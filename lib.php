7350867b6b3b48323eaef450a42ce3fd24b0ca73
Marcus Green <marcus.green@tituslearning.com>
2022-08-14 21:29:08 +0100

    writing back start to db

--- lib.php
+++ lib.php
@@ -23,7 +23,8 @@
  */
 
 defined('MOODLE_INTERNAL') || die();
-
+use \core_availability\info_module;
+use \core_availability\info;
 /**
  * Return if the plugin supports $feature.
  *
@@ -77,7 +78,6 @@ function get_course_contents(int $courseid, array $options): array {
 
 function get_contents_table(MoodleQuickForm $mform, array $contents, \stdClass $current) {
     global $DB;
-    $contentcounter = 0;
     foreach ($contents as $content) {
         if (count($content['modules']) > 0) {
             $group = [];
@@ -101,8 +101,15 @@ function get_contents_table(MoodleQuickForm $mform, array $contents, \stdClass $
     }
     return $mform;
 }
-
-function get_content_data($contents, $current) :array {
+/**
+ * Get the course module items with availability dates calculated based
+ * on supplied form values.
+ *
+ * @param array $contents
+ * @param object $current
+ * @return array
+ */
+function get_content_data(array $contents, stdClass $current) :array {
     $timing['start'] = $current->schedulestart;
     $timing['end'] = $current->schedulefinish;
     $timing['repeatcount'] = $current->repeatcount;
@@ -112,15 +119,17 @@ function get_content_data($contents, $current) :array {
     $sessioncounter = 0;
     foreach ($contents as $content) {
         if (count($content['modules']) > 0) {
-            //$modules = calculate_availabilities($content['modules'], $timing);
-            foreach ($content['modules'] as $module) {
+            $availability = [];
 
+            foreach ($content['modules'] as $module) {
                 if ($contentcounter % ($current->activitiespersession + 1) == 0) {
                     $module = calculate_availability($module, $timing, $sessioncounter);
+                    $availability['start'] = $module['start'];
                     $sessioncounter++;
                     $row['issessionrow'] = true;
                     $row['sessioncounter'] = $sessioncounter;
                     $row['startformatted'] = $module['startformatted'];
+                    $row['start'] = $module['start'];
                     $data['activities'][] = $row;
                     $contentcounter++;
                     continue;
@@ -130,11 +139,14 @@ function get_content_data($contents, $current) :array {
 
                 $questions = $DB->get_records('quiz_slots',['quizid' => $module['instance']]);
                 $details = $DB->get_record($module['modname'], ['id' => $module['instance']]);
-                $availability = $DB->get_record('course_modules', ['id' => $module['instance']], 'availability');
+                //$availability = $DB->get_record('course_modules', ['id' => $module['instance']], 'availability');
+
                 $module['questioncount'] = count($questions);
                 $module['name'] = $details->name;
                 $module['intro'] = strip_tags($details->intro);
                 $module['availability']  = get_availability($module);
+                $module['dripavailability']['start'] = $availability['start'];
+                $module['issessionrow'] = false;
                 $data['activities'][] = $module;
             }
         }
@@ -189,13 +201,41 @@ function get_availability(array $module) {
  * @return bool True if successful, false otherwise.
  */
 function driprelease_update_instance($moduleinstance, $mform = null) {
-    global $DB;
-    if ($data = $mform->get_data()) {
-           $moduleinstance->repeatcount = $data->repeatgroup['repeatcount'];
+    global $DB, $COURSE;
+    $options = [["name" => "modname", "value" => 'quiz']];
+
+    $contents = get_course_contents($COURSE->id, $options);
+    $data = get_content_data($contents, $mform->get_current());
+    update_availability($data['activities']);
+
+    if ($formdata = $mform->get_data()) {
+           $moduleinstance->repeatcount = $formdata->repeatgroup['repeatcount'];
     }
     $moduleinstance->timemodified = time();
     $moduleinstance->id = $moduleinstance->instance;
     return $DB->update_record('driprelease', $moduleinstance);
+}
+/**
+ * $fromform = $mform->get_data();
+ *
+ * @param [type] $data
+ * @return void
+ */
+function update_availability($data) {
+    global $DB;
+
+    foreach ($data as $module) {
+        if (!$module['issessionrow']) {
+            // $modinfo = get_fast_modinfo($COURSE->id, $USER->id);
+            // $info = $modinfo->get_cm($module['id']);
+
+            // $availability = json_decode($info->availability);
+            $s = '{"op":"&","c":[{"type":"date","d":">=","t":'.$module['dripavailability']['start'].'}],"showc":[true]}';
+            $DB->set_field('course_modules', 'availability', $s, ['id' => $module['id']]);
+
+        }
+
+    }
 }
 
 function get_sequence($data) {
